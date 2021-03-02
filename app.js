a21159b1cedfda069dc3b53d7eda9d081e538bfd
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-03-02 15:59:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-// Deconstruct
+// Deconstruct - Spread and Rest Operator
 
-const items = ["banana", "chocolate", "apple", "cheese"];
-
-const [banana, chocolate, apple, cheese] = items;
+// const items = ["banana", "chocolate", "apple", "cheese"];
+// 
+// const [banana, ...rest] = items;
 
 // saved everything to a variable
 
-console.log(banana);
+// console.log(rest);
+// 
+// const newItems = [...items, "peanut"];
+
+// console.log(newItems);
+
+const user = {
+    name: "Kris",
+    photos: [1, 2, 3, 4, 5, 6],
+    age: 44,
+    analytics: {
+        subscribers: 2500,
+        videos: 250
+    }
+};
+
+// const photos = user.analytics.subscribers
+
+// console.log(photos);
+
+const {subscribers} = user.analytics; 
+
+console.log(subscribers);
