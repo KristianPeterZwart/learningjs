ec824c84ffe3ad4e60980f24957a234281ecc232
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-03-02 15:49:48 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const items = ["banana", "chocolate", "apple", "cheese"];
+
+const apple = items[2];
+const apple = items[2];
+const apple = items[2];
+const cheese = itesm[3];
+const cheese = itesm[3];
+const cheese = itesm[3];
+
