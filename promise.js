cc8d4ab1e75dbcd9bb74784245fd1988a13dd42a
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-03-03 10:50:49 +0100

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,12 +1,18 @@
 // Synchronous Code Example
 
-function otherFunct() {
-    console.log("We are in another function");
-    console.log("Do some stuff");
-}
+// function otherFunct() {
+    // console.log("We are in another function");
+    // console.log("Do some stuff");
+// }
+// console.log("Start");
+// otherFunct();
+// console.log("End");
+
+// Asynchronous Code Example
 
 console.log("Start");
-
-otherFunct();
+setTimeout(() => {
+console.log("We are in the timeout")
+}, 5000);
 
 console.log("End");
