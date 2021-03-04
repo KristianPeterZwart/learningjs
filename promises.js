c96afc7977206b8ca9b00a11eeedb4b9194c66fd
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-03-04 11:16:21 +0100

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,14 +1,20 @@
 console.log("Start");
 
-function loginUser(email, password, callback){
+function loginUser(email, password, callback) {
     setTimeout(() => {
         console.log("Now we have the data");
-        return { userEmail: email };
+        callback({ userEmail: email });
     }, 5000);
 }
 
-const user = loginUser = ("krisz@gmail.com", 123456, () => {
+function getUserVideos(email){
+    setTimeout(() => {
+        return ["video1", "video2", "video3"];
+    }, 1000);
+}
 
+const user = loginUser = ("krisz@gmail.com", 123456, user => {
+    console.log(user);
 });
 
 console.log("Finish");
