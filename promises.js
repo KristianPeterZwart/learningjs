872529bc4e41712227634680102ee0bf47b9f739
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-03-04 11:06:15 +0100

    Create promises.js

---
+++ promises.js
@@ -0,0 +1,11 @@
+console.log("Start");
+
+function loginUser(email, password){
+    setTimeout(() => {
+        return [userEmail: email };
+    }, 1500);
+}
+
+const user = loginUser = ("krisz@gmail.com", 123456);
+
+console.log("End");
